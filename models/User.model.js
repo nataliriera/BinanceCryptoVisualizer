3ffb0644dadb9240fc116e1ca1d1549643528976
Natali Riera <nataliriera19@gmail.com>
2022-01-06 22:15:17 -0500

    session.config-cloudinary-errorHandlers-installedPackage

--- models/User.model.js
+++ models/User.model.js
@@ -3,15 +3,32 @@ const { Schema, model } = require("mongoose");
 // TODO: Please make sure you edit the user model to whatever makes sense in this case
 const userSchema = new Schema(
   {
+    
     username: {
       type: String,
+      unique: true,
+      required:[true, "You should set up "]
       // unique: true -> Ideally, should be unique, but its up to you
     },
-    password: String,
-  },
+
+    password: {
+      type: String,
+      required: true,
+    },
+
+    email:{
+      type: String,
+      required: true,
+    },
+
+    profile_picture:{
+      type: String,
+    //default:"http...."
+  }
+},
   {
     // this second object adds extra properties: `createdAt` and `updatedAt`
-    timestamps: true,
+      timestamps: true,
   }
 );
 
